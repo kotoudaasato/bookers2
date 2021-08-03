13f4659e573b5ac3f010bea834ae33fe864397db
kotoudaasato <aru.cam.wanwan@gmail.com>
2021-08-03 15:15:57 +0000

    [Add]favorite

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  resources:books,only: [:create,:index,:show,:edit,:update,:destroy]
+  resources:books,only: [:create,:index,:show,:edit,:update,:destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources:users,only: [:create,:index,:show,:edit,:update,:destroy]
 end
