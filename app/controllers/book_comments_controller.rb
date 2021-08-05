98046172ef36dac0ca5cdf31f33e8ed3e81e73de
kotoudaasato <aru.cam.wanwan@gmail.com>
2021-08-05 08:43:22 +0000

    [修正]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,30 +2,28 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = BookComment.new(book_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    if comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = book.id
+    if @book_comment.save
       redirect_to book_path(book)
     else
       @book=Book.find(params[:book_id])
       @user=@book.user
       @book_new=Book.new
-      @book_comment = BookComment.new
       render :'books/show'
     end
+
 
   end
 
   def destroy
-    book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    book_comment.destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_back(fallback_location: root_path)
   end
 
    private
 
-  def book_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 end
